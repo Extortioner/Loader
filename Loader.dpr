dff215ef6073609cb37ddcb289ee8380fa4a296d
Pavel <dlyafrishek@gmail.com>
2017-04-01 21:01:18 +0300

    multiple copies of app fix

--- Loader.dpr
+++ Loader.dpr
@@ -2,6 +2,7 @@ program Loader;
 
 uses
   Vcl.Forms,
+  Winapi.Windows,
   mainform in 'mainform.pas' {frmMain},
   addproxyform in 'addproxyform.pas' {frmAddProxy},
   proxydestform in 'proxydestform.pas' {frmProxyDestination};
@@ -9,6 +10,12 @@ uses
 {$R *.res}
 
 begin
+  CreateMutex(nil, True, '{CA906874-2065-49F6-A0AE-780520ED7E4C}');
+  if GetLastError = ERROR_ALREADY_EXISTS then
+  begin
+    Application.MessageBox('Программа уже запущена', 'Внимание', MB_OK + MB_ICONINFORMATION);
+    exit;
+  end;
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmMain, frmMain);
